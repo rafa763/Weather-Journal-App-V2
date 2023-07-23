63f3c908d2acccf3f48e29cdf3e98fbb0e496ba6
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-07-24 00:59:59 +0300

    get weather by city name

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Weather-Journal-App-V2
+
 get weather forecast for your city
 <!-- insert gif -->
-<!-- ![weather app](https://thumbs.gfycat.com/RapidDimEider.webp) -->
+![weather app](https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png)
+
 What was i doing wrong?
-- the api key was exposed 
+
+- the api key was exposed
 - werird architecture, idk what i was thinking
 
 what i did to fix it and what is new?
+
 - i used a proxy server to hide the api key
 - the key is now hidden in a .env file
 - responses are now cached to prevent unnecessary api calls
--- src/api/weather.js
+++ src/api/weather.js
@@ -3,8 +3,32 @@ import axios from "axios";
 
 const server = express();
 
-server.get("/", (req, res) => {
-    res.send("weather api");
+let GEO_URL = "http://api.openweathermap.org/geo/1.0/direct"
+let BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+server.post("/", async (req, res) => {
+    try {
+        const { city, state, country } = req.body;
+        const geo = new URLSearchParams({
+            q: `${city},${state},${country}`,
+            limit: 1,
+            appid: process.env.WEATHER_API_KEY
+        });
+        const { data } = await axios.get(`${GEO_URL}?${geo}`);
+
+        const query = new URLSearchParams({
+            lat: data[0].lat,
+            lon: data[0].lon,
+            exclude: "minutely,hourly",
+            units: "metric",
+            appid: process.env.WEATHER_API_KEY
+        });
+
+        const { data: weatherData } = await axios.get(`${BASE_URL}?${query}`);
+        res.status(200).send(weatherData.weather[0]);
+    } catch (error) {
+        throw error;
+    }
 });
 
 export default server;
